61b737277f665c004ed092d1a3e2849474bf58eb
ClaireLC <clairech@stanford.edu>
2024-04-08 11:26:31 -0700

    Add allegro_right config

--- pybullet_robot/src/pybullet_robot/robots/__init__.py
+++ pybullet_robot/src/pybullet_robot/robots/__init__.py
@@ -3,4 +3,4 @@ from .leap_hand.leap_hand_robot import LeapHand
 from .allegro_hand.allegro_hand_config import ROBOT_CONFIG as allegro_config
 from .leap_hand.leap_hand_config import ROBOT_CONFIG as leap_config
 
-robot_configs = {"allegro":allegro_config, "leap":leap_config}
+robot_configs = {"allegro":allegro_config, "leap":leap_config, "allegro_right":allegro_config}
